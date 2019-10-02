3d823398338623349fc977a89cff0cbd8f2ee105
zazaluMonster <hejiajun1432@gmail.com>
2019-10-02 21:58:45 +0800

    backup

--- comment.js
+++ comment.js
@@ -9,7 +9,7 @@ const md5 = require("md5");
 // 根据自己的情况进行配置
 const config = {
     username: "zazaluMonster", // GitHub 用户名
-    token: "d237c8241f425f01d81bc620208ead690e8b250a",  // GitHub Token
+    token: "82bda1ba3abe855414ae179e6f735c14400e8f9f",  // GitHub Token
     repo: "zazaluMonster.github.io",  // 存放 issues的git仓库
     // sitemap.xml的路径，commit.js放置在根目录下，无需修改，其他情况自行处理
     sitemapUrl: path.resolve(__dirname, "./public/sitemap.xml"),
